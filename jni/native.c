2c1088c8a5dc4fac52499e070336b39eb6bbd983
Jakub Galgonek <jakub.galgonek@uochb.cas.cz>
2020-03-22 15:47:07 +0100

    Improve structure search score

--- jni/native.c
+++ jni/native.c
@@ -127,7 +127,7 @@ JNIEXPORT jfloat JNICALL Java_cz_iocb_elchem_molecule_NativeIsomorphism_match(JN
         double heavyBond = molecule->heavyBondCount ? isomorphism->query->heavyBondCount / (double) molecule->heavyBondCount : 1.0;
         double hydrogenBond = molecule->hydrogenBondCount ? isomorphism->query->hydrogenBondCount / (double) molecule->hydrogenBondCount : 1.0;
 
-        score = heavyAtom / 2 + heavyBond / 4 + hydrogenAtom / 8 + hydrogenBond / 16;
+        score = (8 * heavyAtom + 4 * heavyBond + 2 * hydrogenAtom + 1 * hydrogenBond) / 15;
     }
     else if(unlikely(isomorphism->counter == 0))
     {
