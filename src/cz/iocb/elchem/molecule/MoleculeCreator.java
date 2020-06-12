c01cb5c4697cbd81ec5e924565a31d9af45673c6
Juan F. Mosquera <jfmosquera@ebi.ac.uk>
2020-06-12 17:31:50 +0100

    use same logic as query parser

--- src/cz/iocb/elchem/molecule/MoleculeCreator.java
+++ src/cz/iocb/elchem/molecule/MoleculeCreator.java
@@ -139,21 +139,12 @@ public class MoleculeCreator
     public static IAtomContainer getMoleculeFromSmilesOrMolfile(String smilesOrMol, AromaticityMode aromaticityMode)
             throws CDKException, IOException
     {
-        try
-        {
-            return MoleculeCreator.getMoleculeFromMolfile(smilesOrMol, aromaticityMode);
-        }
-        catch(Exception e)
-        {
-            try
-            {
-                return MoleculeCreator.getMoleculeFromSmiles(smilesOrMol, aromaticityMode);
-            }
-            catch(Exception e2)
-            {}
-            throw new CDKException("can not parse molecule as a smiles nor as a molfile");
-        }
+        List<String> lines = Arrays.asList(smilesOrMol.split("\\n"));
 
+        if(lines.size() > 1)
+            return Arrays.asList(MoleculeCreator.getMoleculeFromMolfile(smilesOrMol, aromaticityMode));
+        else
+            return Arrays.asList(MoleculeCreator.getMoleculeFromSmiles(smilesOrMol, aromaticityMode));
     }
 
 
